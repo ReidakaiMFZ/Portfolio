b698d36b20a2007e33a885df661767ee9a31f3a3
Richard <richardbsanches@outlook.com>
2023-06-12 18:13:08 -0300

    initial page almost done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Header from "./components/Header";
 import InitialPage from "./components/InitialPage";
-import About from "./components/About";
 import ProjectList from "./api/Projects";
 
 
@@ -9,9 +8,8 @@ export default function App() {
     return (
         <div className="flex flex-col">
             <Header/>
-            <div className="mt-24">
+            <div className="">
                 <InitialPage/>
-                <About/>
                 <ProjectList/>
             </div>
         </div>
--- src/components/About.tsx
+++
@@ -1,7 +0,0 @@
-export default function About(){
-    return (
-        <div className="bg-red-700 h-screen" id="About">
-            <h1>About</h1>
-        </div>
-    );
-}
