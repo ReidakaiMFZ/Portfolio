488a2b556c31ea7a23bd204abb62df1cd8d39b88
Richard <richardbsanches@outlook.com>
2023-07-02 01:11:07 -0300

    some changes

--- src/components/InitialPage.tsx
+++ src/components/InitialPage.tsx
@@ -1,11 +1,16 @@
 import { FaFileDownload, FaGithub, FaLinkedin } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
+import { Typewriter } from "react-simple-typewriter";
 import curriculum from "../assets/Curriculum.pdf";
+
 export default function InitialPage() {
+
     return(
         <div className="flex flex-col h-screen items-center justify-center" id="Home">
             <h1 className="text-5xl font-bold max-sm:text-3xl">Hi, I am Richard Sanches</h1>
-            <h2 className="text-xl mt-1 max-sm:text-lg">I am a computer science student</h2>
+            <h2 className="text-3xl mt-1 max-sm:text-lg">
+                <Typewriter words={["Fullstack Developer", "Computer Science Student"]} cursor={true} loop={true}/>
+            </h2>
 
             <div className="flex flex-row mt-3 gap-x-2">
                 <a href="https://github.com/reidakaimfz" className=""><FaGithub className="h-16 w-16 max-sm:h-12 max-sm:w-12"/></a>
@@ -13,7 +18,7 @@ export default function InitialPage() {
                 <a href="mailto:richardbsanches@outlook.com">
                     <MdEmail className="w-16 h-16 max-sm:h-12 max-sm:w-12"/>
                 </a>
-                <a className="flex h-16 w-48 items-center justify-center bg-green-500 rounded-full ml-2  max-sm:h-12 max-sm:w-28" download={true} href={curriculum}>
+                <a className="flex h-16 w-48 items-center justify-center bg-green-500 rounded-full ml-2  max-sm:h-12 max-sm:w-28 hover:bg-green-700" download={true} href={curriculum}>
                     <FaFileDownload className="h-16 max-sm:h-6"/>
                     <p className="ml-2 text-md max-sm:text-sm">Curriculum</p>
                 </a>
